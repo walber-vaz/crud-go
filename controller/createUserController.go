9237484e82364d361ec12d907f888df5b73c395b
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-07-17 14:30:38 -0300

    feat: set routes and test default pattern errors

--- controller/createUserController.go
+++ controller/createUserController.go
@@ -1,5 +1,12 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/walber-vaz/backend-go-gin/util/rest_errors"
+)
 
-func CreateUser(ctx *gin.Context) {}
+func CreateUser(ctx *gin.Context) {
+	err := rest_errors.NewBadRequestError("invalid json body")
+	ctx.Header("Content-Type", "application/json")
+	ctx.JSON(err.StatusCode, err)
+}
