18459b2bc09a9e775cb444be9aa0eaf87593f72d
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-07-18 15:28:56 -0300

    feat: create initial function controller

--- model/request/userRequest.go
+++ model/request/userRequest.go
@@ -1,15 +1,13 @@
 package request
 
 type UserRequest struct {
-	Name      string    `json:"name"`
-	Age       int8      `json:"age"`
-	Username  string    `json:"username"`
-	Password  string    `json:"password"`
-	Email     string    `json:"email"`
-	Phone     string    `json:"phone"`
-	Address   []Address `json:"address"`
-	CreatedAt string    `json:"created_at"`
-	UpdatedAt string    `json:"updated_at"`
+	Name     string    `json:"name"`
+	Age      int8      `json:"age"`
+	Username string    `json:"username"`
+	Password string    `json:"password"`
+	Email    string    `json:"email"`
+	Phone    string    `json:"phone"`
+	Address  []Address `json:"address"`
 }
 
 type Address struct {
--- model/response/userResponse.go
+++ model/response/userResponse.go
@@ -3,15 +3,13 @@ package response
 import "github.com/google/uuid"
 
 type UserResponse struct {
-	ID        uuid.UUID `json:"id"`
-	Name      string    `json:"name"`
-	Age       int8      `json:"age"`
-	Username  string    `json:"username"`
-	Email     string    `json:"email"`
-	Phone     string    `json:"phone"`
-	Address   []Address `json:"address"`
-	CreatedAt string    `json:"created_at"`
-	UpdatedAt string    `json:"updated_at"`
+	ID       uuid.UUID `json:"id"`
+	Name     string    `json:"name"`
+	Age      int8      `json:"age"`
+	Username string    `json:"username"`
+	Email    string    `json:"email"`
+	Phone    string    `json:"phone"`
+	Address  []Address `json:"address"`
 }
 
 type Address struct {
