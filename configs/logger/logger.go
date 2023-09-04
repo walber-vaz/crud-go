7db95845c6a96215d7bc073bb5ca865afb579da5
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-09-04 11:26:58 -0300

    doc: gen godoc function logger

--- configs/logger/logger.go
+++ configs/logger/logger.go
@@ -15,6 +15,10 @@ var (
 	LOG_LEVEL  = "LOG_LEVEL"
 )
 
+// init initializes the logging configuration.
+//
+// No parameters.
+// No return types.
 func init() {
 	logConfig := zap.Config{
 		OutputPaths: []string{getOutputLogs()},
@@ -33,17 +37,32 @@ func init() {
 	log, _ = logConfig.Build()
 }
 
+// Info logs the provided message with the given tags as info level.
+//
+// Parameters:
+// - message: the message to be logged (string).
+// - tags: optional tags to be included in the log (zap.Field).
+// Return type: void.
 func Info(message string, tags ...zap.Field) {
 	log.Info(message, tags...)
 	log.Sync()
 }
 
+// Error logs an error message with optional tags.
+//
+// message: the error message to log.
+// err: the error object to log.
+// tags: optional additional tags to include in the log.
 func Error(message string, err error, tags ...zap.Field) {
 	tags = append(tags, zap.NamedError("error", err))
 	log.Info(message, tags...)
 	log.Sync()
 }
 
+// getOutputLogs returns the output logs of the program.
+//
+// It does not take any parameters.
+// It returns a string.
 func getOutputLogs() string {
 	output := strings.ToLower(strings.TrimSpace(os.Getenv(LOG_OUTPUT)))
 	if output == "" {
@@ -53,6 +72,10 @@ func getOutputLogs() string {
 	return output
 }
 
+// getLevelLogs returns the zapcore.Level based on the value of the LOG_LEVEL environment variable.
+//
+// It takes no parameters.
+// It returns a zapcore.Level.
 func getLevelLogs() zapcore.Level {
 	switch strings.ToLower(strings.TrimSpace(os.Getenv(LOG_LEVEL))) {
 	case "info":
